17f4f3beebba1d7b0b3f3a3471bdfdc8fe04ef3b
Axarvia <alexandrelopesconnection@gmail.com>
2023-03-20 20:36:33 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+import Sidebar from './Sidebar';
+import Main from './Main';
+import { useEffect, useState } from 'react';
+import uuid from 'react-uuid';
+
+
+function App() {
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []);
+  const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+
+  const onAddNote = () => {
+    const newNote ={
+      id:uuid(),
+      title: "Untitled",
+      body: "",
+      lastModified: Date.now(),
+    }
+
+    setNotes([newNote, ...notes]);
+  };
+
+  const onDeleteNote = (idToDelete) => {
+    setNotes(notes.filter((note) => note.id !== idToDelete))
+  };
+
+  const getActiveNote = () => {
+    return notes.find((note)=> note.id === activeNote);
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    const updateNotesArray = notes.map((note) => {
+      if(note.id === activeNote){
+        return updatedNote;
+      }
+      return note;
+    })
+
+    setNotes(updateNotesArray);
+
+  }
+
+// End of Functions, start of Render
+
+  return (
+    <div className="App">
+    <Sidebar notes={notes} 
+    onAddNote={onAddNote} 
+    onDeleteNote={onDeleteNote}
+    activeNote={activeNote}
+    setActiveNote={setActiveNote}
+    />
+    <Main 
+    activeNote={getActiveNote()}
+    onUpdateNote={onUpdateNote}
+    />
+   </div>
+  );
+}
+
+export default App;
